7e63c4e9d2e0d233bcdc7a7bcd4239c63681c385
joshseyda <josh.seyda@gmail.com>
2018-07-08 17:05:22 -0400

    tweaking config

--- src/App.js
+++ src/App.js
@@ -12,6 +12,7 @@ class App extends Component {
             <h1 className = "App-title" > Welcome to React </h1> </header > <p className = "App-intro" >
             To get started, edit < code > src / App.js </code> and save to reload. </p > </div> */}
             <h1>Welcome to my first React App!</h1>
+            </div>
         );
     }
 }
