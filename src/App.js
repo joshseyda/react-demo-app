346f3af00f35068a2d56861872351a38189abe04
joshseyda <josh.seyda@gmail.com>
2018-07-08 18:09:37 -0400

    shuffling around JSX

--- src/App.js
+++ src/App.js
@@ -5,14 +5,12 @@ import './App.css';
 class App extends Component {
     render() {
         return ( < div className = "App" >
-            {/* <header className = "App-header" >
+             <header className = "App-header" >
             <img src = { logo }
             className = "App-logo"
             alt = "logo" />
-            <h1 className = "App-title" > Welcome to React </h1> </header > <p className = "App-intro" >
-            To get started, edit < code > src / App.js </code> and save to reload. </p > </div> */}
-            <h1>Welcome to my first React App!</h1>
-            </div>
+            <h1 className = "App-title" > React App v.0.0.1 </h1> </header > <p className = "App-intro" >
+            Soon to come more FN details. </p > </div> 
         );
     }
 }
